65be969acc0042dfa45ddc4de6465e89f8ba2f08
polyprogrammist <pechkin350@gmail.com>
2018-11-09 23:04:56 +0300

    try user_id print

--- api_handlers/user_create_handler.py
+++ api_handlers/user_create_handler.py
@@ -7,5 +7,25 @@ class UserCreateHandler(RequestHandler):
         self.post()
 
     def post(self):
+        data = self.request.body
+        print(data['user_id'])
+        query = """
+        insert into users(
+            user_id, 
+            first_name, 
+            last_name, 
+            birthday, 
+            photo
+            ) 
+            values(
+            2, 
+            'slava', 
+            'vushev', 
+            '20000505', 
+            'user1.jpg'
+            );
+            """
+        print(query)
         self.write(self.request.body)
         #self.write(json.dumps({'result': 'ok'}))
+
