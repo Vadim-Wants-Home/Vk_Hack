9fb7b50a79e45bbe6db31ecfa04ad45c797ebd77
polyprogrammist <pechkin350@gmail.com>
2018-11-10 02:08:09 +0300

    rel0.2

--- api_handlers/user_create_handler.py
+++ api_handlers/user_create_handler.py
@@ -10,15 +10,14 @@ class UserCreateHandler(RequestHandler):
         self.post()
 
     def post(self):
-        print(self.request.body)
         data = json.loads(self.request.body)
-        print(data)
         query = """
         insert into users(user_id, first_name, last_name, birthday, photo) 
         values({user_id}, '{first_name}', '{last_name}', '{birthday}', '{photo}');
             """\
             .format(**data)
-        print(query)
-        self.db.insert_query(query)
-        self.write(json.dumps({'result': 'ok'}))
-
+        try:
+            self.db.insert_query(query)
+            self.write(json.dumps({'result': 'ok'}))
+        except:
+            self.write(json.dumps({'result': 'fail'}))
