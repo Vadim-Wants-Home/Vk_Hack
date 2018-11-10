aa817cb8d9fa429abad145580d17620a82ae5939
polyprogrammist <pechkin350@gmail.com>
2018-11-11 02:13:33 +0300

    server running correctly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tornado.ioloop
 import tornado.web
+import logging
 import torndb
 import sys
 
@@ -37,6 +38,8 @@ if __name__ == "__main__":
     SqlDatasetWorker.uninstall()
     SqlDatasetWorker.install()
     SqlDatasetWorker.sample_data_insert()
+
+    logging.warn('server running correctly')
 
     app = make_app()
     app.listen(get_json_data('server.json')['port'])
